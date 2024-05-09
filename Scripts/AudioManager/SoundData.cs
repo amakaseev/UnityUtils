0b4e0848877c28debd3ff583caf09ba5ff6780fe
amakaseev <aleksey.makaseev@gmail.com>
2024-05-09 15:05:20 +0300

    refactor SoundData

--- Scripts/AudioManager/SoundData.cs
+++ Scripts/AudioManager/SoundData.cs
@@ -5,15 +5,12 @@ namespace UnityUtils {
   [CreateAssetMenu(fileName = "SoundData", menuName = "AudioManager/SoundData")]
   public class SoundData: ScriptableObject {
     public AudioClip sound;
-    public bool randomPitch = true;
-    public bool randomVolume = true;
-    [Range(0, 1)] public float sameTime = 0.1f;
-    [Range(0, 1)] public float volume = 1;
+    [Range(0.1f, 1)] public float randomVolume = 0.2f;
+    [Range(0.1f, 1)] public float sameTime = 0.2f;
+    [Range(0.1f, 1)] public float volume = 1;
 
     public void Play(AudioSource audioSource) {
-      audioSource.pitch = randomPitch ? 1 : Random.Range(0.95f, 1.05f);
-      audioSource.PlayOneShot(sound, volume * (randomVolume? Random.Range(0.9f, 1f) : 1));
-      audioSource.pitch = 1;
+      audioSource.PlayOneShot(sound, volume * Random.Range(1f - randomVolume, 1f));
     }
   }
 
