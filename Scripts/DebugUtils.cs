04da7757b8a9ad400b6e0bae97a3b2e7418d00ba
amakaseev <aleksey.makaseev@gmail.com>
2024-06-11 12:15:20 +0300

    Revert "Update DebugUtils.cs"
    This reverts commit 224c6d01a9af0dd0d3457a8be095a2ffb2f5dd6c.

--- Scripts/DebugUtils.cs
+++ Scripts/DebugUtils.cs
@@ -3,11 +3,11 @@
 public static class DebugUtils {
 
   public static void Log(string text, Color color) {
-    Debug.Log($"<font color=#{ColorUtility.ToHtmlStringRGB(color)}>{text}</font>");
+    Debug.Log($"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{text}</color>");
   }
 
   public static void Log(string prefix, Color color, string text) {
-    Debug.Log($"<font color=#{ColorUtility.ToHtmlStringRGB(color)}>{prefix}</font>{text}");
+    Debug.Log($"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{prefix}</color>{text}");
   }
 
   public static void LogFSM(string text) {
